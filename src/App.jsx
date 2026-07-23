4a8647e02aa0c40aa90ab8243b2f3515d49aa747
okistaylor <72969270+TaylorOkis@users.noreply.github.com>
2026-07-23 10:19:36 +0100

    Add Image to page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import loginImageOne from "./assets/Login Art.png";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="image-container">
+        <img src={loginImageOne} alt="login image" width="" height="" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
