4c0360a67a6f1f21ba8a4bc5d92d2acb7ae006ad
okistaylor <72969270+TaylorOkis@users.noreply.github.com>
2026-07-23 11:02:13 +0100

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import loginImageOne from "./assets/Login Art.png";
 
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="wrapper">
       <div className="image-container">
-        <img src={loginImageOne} alt="login image" width="" height="" />
+        <img src={loginImageOne} alt="login image" width="816" height="1076" />
       </div>
+
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <>
+      <h1>Welcome Back 👋</h1>
+      <div>
+        <p>Today is a new day. It's your day. You shape it.</p>
+        <p>Sign in now to start managing your projects</p>
+      </div>
+    </>
+  );
+}
